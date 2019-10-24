98ba0bd34db4f06602cfefd0f09357c677dfc3c9
stupendous-patch-9345 <yo.park93@yahoo.com>
2019-10-24 21:00:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,8 @@
 def find_element_index(array, value_to_find)
  array.length.times do |index| 
    if array[index] == value_to_find
- puts array[value_to_find] 
+     return count
+     
 
 
 def find_max_value(array)
